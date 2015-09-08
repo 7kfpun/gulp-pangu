d5c97adfad959827d8efd7268c9bd16cf9af146f
kf <7kfpun@gmail.com>
2015-09-09 01:10:51 +0800

    update algorithm

--- index.js
+++ index.js
@@ -31,61 +31,30 @@ var PLUGIN_NAME = 'gulp-pangu';
 
 var pangu = function() {
 
-  // https://github.com/vinta/paranoid-auto-spacing/blob/master/src/pangu.js#L72-L130
+  // https://github.com/vinta/paranoid-auto-spacing/blob/master/dist/pangu.js#L43-L68
   function insert_space(text) {
     var old_text = text;
     var new_text;
-
-    /*
-      Regular Expressions
-      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-      Symbols
-      ` ~ ! @ # $ % ^ & * ( ) _ - + = [ ] { } \ | ; : ' " < > , . / ?
-      3000-303F 中日韓符號和標點
-      3040-309F 日文平假名 (V)
-      30A0-30FF 日文片假名 (V)
-      3100-312F 注音字母 (V)
-      31C0-31EF 中日韓筆畫
-      31F0-31FF 日文片假名語音擴展
-      3200-32FF 帶圈中日韓字母和月份 (V)
-      3400-4DBF 中日韓統一表意文字擴展 A (V)
-      4E00-9FFF 中日韓統一表意文字 (V)
-      AC00-D7AF 諺文音節 (韓文)
-      F900-FAFF 中日韓兼容表意文字 (V)
-      http://unicode-table.com/cn/
-      */
-
-    // 前面"中間"後面 >> 前面 "中間" 後面
-    text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])(["])/ig, '$1 $2');
-    text = text.replace(/(["])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
-
-    // 避免出現「前面 " 中間" 後面」之類的不對稱的情況
-    text = text.replace(/(["']+)(\s*)(.+?)(\s*)(["']+)/ig, '$1$3$5');
-
-    // # 符號需要特別處理
-    text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])(#(\S+))/ig, '$1 $2');
-    text = text.replace(/((\S+)#)([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $3');
-
-    // 前面<中間>後面 --> 前面 <中間> 後面
+    text = text.replace(/([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])(["])/g, '$1 $2');
+    text = text.replace(/(["])([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/g, '$1 $2');
+    text = text.replace(/(["'\(\[\{<\u201c]+)(\s*)(.+?)(\s*)(["'\)\]\}>\u201d]+)/g, '$1$3$5');
+    text = text.replace(/([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])( )(')([A-Za-z])/g, '$1$3$4');
+    text = text.replace(/([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])(#(\S+))/g, '$1 $2');
+    text = text.replace(/((\S+)#)([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/g, '$1 $3');
+    text = text.replace(/([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([\+\-\*\/=&\\|<>])([A-Za-z0-9])/g, '$1 $2 $3');
+    text = text.replace(/([A-Za-z0-9])([\+\-\*\/=&\\|<>])([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/g, '$1 $2 $3');
     old_text = text;
-    new_text = old_text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([<\[\{\(\u201c]+(.*?)[>\]\}\)\u201d]+)([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2 $4');
+    new_text = old_text.replace(/([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([\(\[\{<\u201c]+(.*?)[\)\]\}>\u201d]+)([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/g, '$1 $2 $4');
     text = new_text;
     if (old_text === new_text) {
-      // 前面<後面 --> 前面 < 後面
-      text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([<>\[\]\{\}\(\)\u201c\u201d])/ig, '$1 $2');
-      text = text.replace(/([<>\[\]\{\}\(\)\u201c\u201d])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
+        text = text.replace(/([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([\(\[\{<\u201c>])/g, '$1 $2');
+        text = text.replace(/([\)\]\}>\u201d<])([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/g, '$1 $2');
     }
-    // 避免出現「前面 [ 中間] 後面」之類的不對稱的情況
-    text = text.replace(/([<\[\{\(\u201c]+)(\s*)(.+?)(\s*)([>\]\}\)\u201d]+)/ig, '$1$3$5');
-
-    // 中文在前
-    text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([a-z0-9`~@\$%\^&\*\-\+=\|\\\/\u0080-\u00ff\u2022\u2150-\u218f])/ig, '$1 $2');
-
-    // 中文在後
-    text = text.replace(/([a-z0-9`~!\$%\^&\*\-\+=\|\\;\:\,\.\/\?\u0080-\u00ff\u2022\u2150-\u218f])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
-
-    // 避免「陳上進's something」的 's 前面被加了空格
-    text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])( )(')([a-z])/ig, '$1$3$4');
+    text = text.replace(/([\(\[\{<\u201c]+)(\s*)(.+?)(\s*)([\)\]\}>\u201d]+)/g, '$1$3$5');
+    text = text.replace(/([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([~!;:,\.\?\u2026])([A-Za-z0-9])/g, '$1$2 $3');
+    text = text.replace(/([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([A-Za-z0-9`\$%\^&\*\-=\+\\\|/@\u00a1-\u00ff\u2022\u2027\u2150-\u218f])/g, '$1 $2');
+    text = text.replace(/([A-Za-z0-9`~\$%\^&\*\-=\+\\\|/!;:,\.\?\u00a1-\u00ff\u2022\u2026\u2027\u2150-\u218f])([\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/g, '$1 $2');
+    text = text.replace(/(Taipei)(,)(China)/g, '$1$2 $3');
 
     return text;
   }
@@ -102,7 +71,7 @@ var pangu = function() {
 
     if (file.isBuffer()) {
       file.contents = Buffer.concat([
-        new Buffer(insert_space(file.contents.toString()))
+        new Buffer(insert_space(insert_space(file.contents.toString())))
       ]);
     }
 
